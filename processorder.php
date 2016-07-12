25c0cfef02e2c55fde1a830d51d70f9cffbcc6f5
zhou cheng <zhoucheng@staff.sina.com.cn>
2016-07-12 18:07:16 +0800

    Order test

---
+++ processorder.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Bob 's Auto Parts - Order Results</title>
+</head>
+<body>
+	<h1>
+		Bob  Auto parts
+	</h1>
+	<h2>
+		Oder Results
+		<?php
+			echo '<p> Order processed. </p>'
+		?>
+	</h2>
+
+</body>
+</html>
