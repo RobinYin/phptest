a9985e76e626365f02798415f310767bb66ece89
RobinYin <iyinrongbin@gmail.com>
2016-07-27 18:19:39 +0800

    add mysql search results

---
+++ results.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>
+		Book-O-Rama Search Results
+	</title>
+</head>
+
+<body>
+	<h1>Book-O-Rama Search Results</h1>
+
+	<?php
+	$searchtype = $_POST['searchtype'];
+	$searchterm = $_POST['searchterm'];
+
+	if(!$searchtype || !$searchterm){
+		echo 'You have not entered search details. Please go back and try again:';
+		exit;
+	}
+
+	if(!get_magic_quotes_gpc){
+		$searchtype = addslashes($searchtype);
+		$searchterm = addslashes($searchterm);
+	}
+
+	@ $db = new mysqli('localhost','bookorama','bookorama123','books');
+
+	if(mysqli_connect_errno()){
+		echo 'Error: Counld not connect to database. Please try again later.';
+		exit;
+	}
+
+	$query = "select * from books where "."$searchtype"." like '%".$searchterm."%'";
+
+	$result = $db->query($query);
+
+	$num_results = $result->num_rows;
+
+	echo "<p>Number of books found:".$num_results."</p>";
+
+	for($i=0; $i < $num_results; $i++){
+		$row = $result->fetch_assoc();
+		echo "<p><strong>".($i+1).".Title:";
+		echo htmlsepcialchars(stripslashes($row['title']));
+		echo "</strong><br />Author:";
+		echo stripslashes($row['author']);
+		echo "<br />ISBN:";
+		echo stripslashes($row['isbn']);
+		echo "<br />Price:";
+		echo stripslashes($row['price']);
+		echo "</p>";
+	}
+
+	$result->free();
+	$db->close();
+	?>
+
+</body>
+
+</html>
