e9a0b3baaade10e89a7b4801f366fdb82265ec91
RobinYin <iyinrongbin@gmail.com>
2016-08-08 11:27:32 +0800

    session fix php end char

--- sess_page1.php
+++ sess_page1.php
@@ -5,6 +5,6 @@
 	$_SESSION['sess_var'] = "Hellow world!";
 
 	echo 'The content of $_SESSION[\'sess_var\'] is ' .$_SESSION['sess_var'].'<br />';
-
+?>
 	<a href="sess_page2.php">Next page</a>
 
--- sess_page2.php
+++ sess_page2.php
@@ -7,5 +7,5 @@
 	echo 'The content of $_SESSION[\'sess_var\'] is ' .$_SESSION['sess_var'].'<br />';
 
 	unset($_SESSION['sess_var']);
-
+?>
 	<a href="sess_page3.php">Next page</a>
